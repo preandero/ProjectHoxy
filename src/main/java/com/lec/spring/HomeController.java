eb7cf501b39a40309ccbc177a4f82b087656f062
preandero <preandero@gmail.com>
2020-07-14 16:32:15 +0900

    test

---
+++ src/main/java/com/lec/spring/HomeController.java
@@ -0,0 +1,17 @@
+package com.lec.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(Model model){
+        model.addAttribute("greeting", "hello0");
+
+        return "home";
+    }
+
+}
