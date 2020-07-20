04876b50185e414b657809d109a37be3e3513392
jiwoo-yoon <lsvn1541@naver.com>
2020-07-20 16:25:40 +0900

    폴더명변경2

--- src/main/java/com/lec/auth/NaverAPI20.java
+++ src/main/java/com/lec/auth/NaverAPI20.java
@@ -1,25 +1,25 @@
-package com.lec.auth;
-
-import com.github.scribejava.core.builder.api.DefaultApi20;
-
-public class NaverAPI20 extends DefaultApi20 implements SnsUrls {
-   private NaverAPI20(){}
-
-    @Override
-    public String getAccessTokenEndpoint() {
-        return NAVER_ACCESS_TOKEN;
-    }
-
-    @Override
-    protected String getAuthorizationBaseUrl() {
-        return NAVER_AUTH;
-    }
-
-    private static class InsatanceHolder{
-       private static final NaverAPI20 INSTANCE = new NaverAPI20();
-   }
-   public static NaverAPI20 getInstance(){
-       return InsatanceHolder.INSTANCE;
-   }
-
-}
+//package com.lec.auth;
+//
+//import com.github.scribejava.core.builder.api.DefaultApi20;
+//
+//public class NaverAPI20 extends DefaultApi20 implements SnsUrls {
+//   private NaverAPI20(){}
+//
+//    @Override
+//    public String getAccessTokenEndpoint() {
+//        return NAVER_ACCESS_TOKEN;
+//    }
+//
+//    @Override
+//    protected String getAuthorizationBaseUrl() {
+//        return NAVER_AUTH;
+//    }
+//
+//    private static class InsatanceHolder{
+//       private static final NaverAPI20 INSTANCE = new NaverAPI20();
+//   }
+//   public static NaverAPI20 getInstance(){
+//       return InsatanceHolder.INSTANCE;
+//   }
+//
+//}
