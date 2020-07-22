b6a91d49c6294c087cfd5165f1ca21cd4c5815ca
preandero <preandero@gmail.com>
2020-07-22 16:08:25 +0900

    Suu Commit4 20/07/22

--- src/main/java/com/lec/spring/resume/controller/resumeController.java
+++ src/main/java/com/lec/spring/resume/controller/resumeController.java
@@ -5,6 +5,11 @@ import com.lec.spring.resume.domain.photoForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.io.File;
 
 @Controller
 @RequestMapping("/resume")
@@ -17,10 +22,26 @@ public class resumeController {
         return "resume/resumeWrite";
     }
 
-    @RequestMapping (value = "addaction.do")
-    public String add_action (Model model, photoForm form) {
+
+    @RequestMapping(value = "/upload" , method = RequestMethod.POST)
+    public String upload(MultipartHttpServletRequest mtf) throws Exception {
+        // 파일 태그
+        String fileTag = "file";
+        // 업로드 파일이 저장될 경로
+        String filePath = "../profile/" + mtf.getContextPath();
+        // 파일 이름
+        MultipartFile file = mtf.getFile(fileTag);
+        String fileName = ((MultipartFile) file).getOriginalFilename();
+        // 파일 전송
+        try {
+            file.transferTo(new File(filePath + fileName));
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("업로드 오류");
+        }
 
         return "";
+
     }
 
 
