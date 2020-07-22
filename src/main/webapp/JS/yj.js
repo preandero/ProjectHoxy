7536ee542d3ec991604fa82bbf3de475855321f4
NAMYOONJOO <bhd4445@naver.com>
2020-07-22 09:16:36 +0900

    2020 07 22 main (yj)

---
+++ src/main/webapp/JS/yj.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $("#header_sub_nav1").hide();
+    $("#header_sub_a1").mouseenter(function() {
+        $("#header_sub_nav1").show();
+    });
+    $("#header_sub_nav1").mouseleave(function() {
+        $("#header_sub_nav1").hide();
+
+    });
+
+});
