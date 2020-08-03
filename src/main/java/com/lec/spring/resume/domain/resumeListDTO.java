f8335aca6023d49e27aa6ac0c2506f3697c07beb
preandero <preandero@gmail.com>
2020-08-03 16:19:51 +0900

    20/08/03 Suu

---
+++ src/main/java/com/lec/spring/resume/domain/resumeListDTO.java
@@ -0,0 +1,32 @@
+package com.lec.spring.resume.domain;
+
+import java.util.Date;
+
+public class resumeListDTO {
+    private String r_title;
+    private Date r_date;
+
+    public resumeListDTO(String r_title, Date r_date) {
+        this.r_title = r_title;
+        this.r_date = r_date;
+    }
+
+    public resumeListDTO() {
+    }
+
+    public String getR_title() {
+        return r_title;
+    }
+
+    public void setR_title(String r_title) {
+        this.r_title = r_title;
+    }
+
+    public Date getR_date() {
+        return r_date;
+    }
+
+    public void setR_date(Date r_date) {
+        this.r_date = r_date;
+    }
+}
